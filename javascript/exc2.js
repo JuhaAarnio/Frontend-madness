f59bdf9ed28b32548c11ff047f46b81e23303f7f
Juha Aarnio <juha.aarnio@live.fi>
2019-11-21 19:01:58 +0200

    Update exc2.js
    Regular expressions are now used to check if the barcode is valid

--- javascript/exc2.js
+++ javascript/exc2.js
@@ -1,27 +1,50 @@
 function Decode() {
     let virtualBarcode = document.getElementById('virtual_barcode').value;
-    let version = virtualBarcode.substring(0,1);
-    let iban_area = document.getElementById('payee');
-    let amount_area = document.getElementById('amount');
-    let reference_area = document.getElementById('payment_reference');
-    let date_area = document.getElementById('due_date');
-
-    if (version==="4"){
-        let iban = document.createTextNode(virtualBarcode.substring(1,17));
-        let euros = document.createTextNode(virtualBarcode.substring(17,23) + ":" + virtualBarcode.substring(23,25));
-        let reference = document.createTextNode(virtualBarcode.substring(28,43));
-        let due_date = document.createTextNode(virtualBarcode.substring(44,50));
+    let isValid = checkBarcode(virtualBarcode);
+    if (virtualBarcode.length === 54 && isValid === true){
+        let version = virtualBarcode.substring(0,1);
+        let iban_area = document.getElementById('payee');
+        let amount_area = document.getElementById('amount');
+        let reference_area = document.getElementById('payment_reference');
+        let date_area = document.getElementById('due_date');
+        let iban, euros, reference, due_date;
+        if (version==="4"){
+            iban = document.createTextNode(virtualBarcode.substring(1,17));
+            euros = document.createTextNode(virtualBarcode.substring(17,23) + "," + virtualBarcode.substring(23,25));
+            reference = document.createTextNode(virtualBarcode.substring(26,45));
+            due_date = document.createTextNode(virtualBarcode.substring(45,51));
+        }
+        if (version==="5") {
+            iban = document.createTextNode(virtualBarcode.substring(1, 17));
+            euros = document.createTextNode(virtualBarcode.substring(17, 23) + "," + virtualBarcode.substring(23, 25));
+            reference = document.createTextNode(virtualBarcode.substring(25, 48));
+            due_date = document.createTextNode(virtualBarcode.substring(48, 54));
+        }
+        else{
+            console.log("Erraneous version");
+        }
         iban_area.appendChild(iban);
         amount_area.appendChild(euros);
         reference_area.appendChild(reference);
         date_area.appendChild(due_date);
     }
-    if (version==="5"){
+    else if (isValid === false) {
+        console.log("Invalid characters in barcode!")
+    }
+    else if (virtualBarcode.length !== 54){
+        console.log("Invalid barcode")
+    }
+}
 
+function checkBarcode(virtualBarcode) {
+    let i;
+    let reg = new RegExp('^[0-9]+$');
+    for(i=0; i < virtualBarcode.length; i++){
+        if (!virtualBarcode[i].match(reg)){
+            return false;
+        }
     }
-    else{
-        console.log("Erraneous version");
-    }
+    return true;
 }
 
 function Hide()  {
